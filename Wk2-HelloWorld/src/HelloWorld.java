d0a1f71d97e7aaffe63c69410952bc68a49df389
MagicalAsh <wesleyrogers01@gmail.com>
2015-08-27 15:51:13 -0400

    First Stuff, yay!

---
+++ Wk2-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+    Written by Wesley Rogers
+    Please see the license file at the top level of
+    my Github (https://github.com/WesleyRogers/AP-CompSci) account associated with my AP Computer
+    Science A for licensing issues.
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+System.out.println("Hello World! I'm using Eclipse!");
+	}
+
+}
