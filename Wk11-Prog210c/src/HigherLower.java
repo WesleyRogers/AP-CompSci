5920589dd666726954828c4b4c8a52e020f341f7
WesleyRogers <wroge7934@fcschools.net>
2015-11-03 10:34:56 -0500

    Finished two programs.

---
+++ Wk11-Prog210c/src/HigherLower.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HigherLower {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int randInt = (new Random()).nextInt(100) + 1; //Create an instance of the random class, then get a new int from it.
+        int lastGuess;
+        int guessNum = 0;
+        System.out.println("I'm thinking of a number between 1 and 100...");
+        do {
+            System.out.print("Enter your guess: ");
+            lastGuess = input.nextInt();
+            guessNum++;
+            if(lastGuess == randInt){
+                System.out.println("You won in " + guessNum + " guesses!");
+            } else if (lastGuess > randInt){
+                System.out.println("You're a bit high. Try lower.");
+            } else {
+                System.out.println("You're a bit low. Try higher.");
+            }
+            
+        } while (lastGuess != randInt);
+        input.close();
+    }
+
+}
+/* Sample Output
+I'm thinking of a number between 1 and 100...
+Enter your guess: 1
+You're a bit low. Try higher.
+Enter your guess: 50
+You're a bit low. Try higher.
+Enter your guess: 75
+You're a bit high. Try lower.
+Enter your guess: 60
+You're a bit high. Try lower.
+Enter your guess: 55
+You're a bit high. Try lower.
+Enter your guess: 51
+You're a bit low. Try higher.
+Enter your guess: 52
+You're a bit low. Try higher.
+Enter your guess: 53
+You won in 8 guesses!
+*/
